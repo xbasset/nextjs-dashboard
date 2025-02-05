d42024328106e0758ed53a670b140fcf92cc2aa8
xbasset <xavier@xbasset.com>
2025-02-05 20:03:52 +0100

    fix delete action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,8 +69,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice.');
-    
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
